b3d792aa133ef8f9509d721ff179f373615c7e4f
unknown <BENUTZEN VERBOTEN@.(none)>
2010-10-09 11:22:25 +0200

    Added Menü

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,39 +1,93 @@
-#include "Framework.hpp"
-#include "Game.hpp"
-#include "Punkte.hpp"
-#include "Player.hpp"
+  #include "Framework.hpp"
+  #include "Game.hpp"
+  #include "Punkte.hpp"
+  #include "Player.hpp"
+  #include <iostream>
+  #include <fstream>
 
 using namespace std;
 
 int main (int argc, char *argv[])
 {
+	char auswahl;
 
   // Framework initialisieren
-  if (g_pFramework->Init (800, 600, 16, true) == false)
-    return (0);
-  
+
   // Neue Instanz des Spiels
-   CGame Game;
-
+	
   // Spiel initialisieren
-  Game.Init ();
-
+	
   // Spiel laufen lassen. Diese Funktion läuft so lange,
   // bis das Spiel geschlossen oder die Escape-Taste
   // gedrückt wird
-  Game.Run ();
+   int highscore = 0;
+  	
+	do
+	{
+	cout << "Asteroids - Menue" << endl;
+	cout << "-------------------" << endl;
+	cout << "(N)eus Spiel starten" << endl;
+	cout << "(H)ighscore anzeigen" << endl;
+	cout << "(I)nfo" << endl;
+	cout << "(B)eenden\n\n";
+//Abfrage
+	cout << "Auswahl ";
+	cin >> auswahl;
 
-  // Spiel beenden
-  Game.Quit ();
+	switch (auswahl) {
+	
+		case ('N'):
+		case ('n'):  {
+			if (g_pFramework->Init (800, 600, 16, true) == false)
+				return (0);
+			CGame Game;
+			Game.Init ();
+			Game.Run ();
 
-  //m_pSpielerpunkte->m_zeigePunkte ();
-  		//Punkte anzeigen
-	    CPunkte::Get()->m_zeigePunkte();
+			// Spiel beenden
+			Game.Quit ();
+
+
  
-  // Framework beenden
-  g_pFramework->Quit ();
-  g_pFramework->Del ();
+		  // Framework beenden
+			g_pFramework->Quit ();
+			g_pFramework->Del ();
+		} break;
+		
+		case ('H'):
+		case ('h'): {
+			
+			ifstream Input ("Highscore.hsc", ios::binary);
+			if (Input == NULL) {
+					cout << "Datei konnte nicht geoefnet werden" << endl;
+			} else {
+				Input.read ((char*) &highscore, sizeof (highscore));
+				Input.close ();
+				cout << "Der Highscore ist: ";
+				cout << highscore << endl;
+			}
+		} break;
 
-  return (0);
+		case ('I'):
+		case ('i'): {
+			cout << "\nAsteroids v1.0\n";
+			cout << "Copyright Lennart Haller\n\n";
+					} break;
+		
+		
+		
+		
+		default:
 
+	{
+		cout << "Falsche Eingabe!\n\n";
+	}
+   }
+    } while (auswahl != 'b' && auswahl != 'B');
+	
+	cout << "Bis zum nächsten Mal" << endl;
+
+				
+		return 0;
 }
+
