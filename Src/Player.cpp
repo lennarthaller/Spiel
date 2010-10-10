lennarthaller/Spiel

commit 427946b606e9ec6f7eca0cb9259dd55f27b728a7

Steuerung Diagonal ermöglicht Wiederauftauchen am gegenüberliegenden Bildschirmrand

Src/Player.cpp
@@ -187,7 +187,7 @@ void CPlayer::ProcessMoving ()
 
   }
   // Nach rechts?
-  else if (g_pFramework->KeyDown (SDLK_RIGHT))
+  if (g_pFramework->KeyDown (SDLK_RIGHT))
   {
     // Spieler nach rechts bewegen
     m_fXPos += 300.0f * g_pTimer->GetElapsed ();
@@ -198,7 +198,7 @@ void CPlayer::ProcessMoving ()
   }
   
   // Nach oben?
-  else if (g_pFramework->KeyDown (SDLK_UP))
+   if (g_pFramework->KeyDown (SDLK_UP))
   {
     // Spieler nach oben bewegen
     m_fYPos -= 300.0f * g_pTimer->GetElapsed ();
@@ -207,7 +207,7 @@ void CPlayer::ProcessMoving ()
 
   
   // Nach unten?
-  else if (g_pFramework->KeyDown (SDLK_DOWN))
+   if (g_pFramework->KeyDown (SDLK_DOWN))
   {
     // Spieler nach unten bewegen
     m_fYPos += 300.0f * g_pTimer->GetElapsed ();
@@ -265,9 +265,15 @@ void CPlayer::CheckPosition ()
 {
   // Linker und rechter Rand
   if (m_fXPos < 0.0f)
-    m_fXPos = 0.0f;
-  else if (m_fXPos > 752.0f)
     m_fXPos = 752.0f;
+  else if (m_fXPos > 752.0f)
+    m_fXPos = 0.0f;
+  
+    if (m_fYPos < 0.0f)
+    m_fYPos = 542.0;
+  else if (m_fYPos > 542.0f)
+    m_fYPos = 0.0f;
+
 
   // Animationsphase prüfen
   if (m_fAnimPhase < 0.0f)

commit 4494b24869f69be47cd5513ca25b438147f11de4

Fixed regression introduced in 427946b606e9ec6f7ec

Src/Player.cpp
@@ -176,6 +176,8 @@ void CPlayer::Update ()
 //
 void CPlayer::ProcessMoving ()
 {
+  bool bHasMoved = false;
+
   // Nach links?
   if (g_pFramework->KeyDown (SDLK_LEFT))
   {
@@ -185,6 +187,7 @@ void CPlayer::ProcessMoving ()
     // Animieren
     m_fAnimPhase -= 20.0f * g_pTimer->GetElapsed ();
 
+	bHasMoved = true;
   }
   // Nach rechts?
   if (g_pFramework->KeyDown (SDLK_RIGHT))
@@ -195,27 +198,30 @@ void CPlayer::ProcessMoving ()
     // Animieren
     m_fAnimPhase += 20.0f * g_pTimer->GetElapsed ();
 
+	bHasMoved = true;
   }
   
   // Nach oben?
-   if (g_pFramework->KeyDown (SDLK_UP))
+  if (g_pFramework->KeyDown (SDLK_UP))
   {
     // Spieler nach oben bewegen
     m_fYPos -= 300.0f * g_pTimer->GetElapsed ();
 
+	bHasMoved = true;
   }
 
   
   // Nach unten?
-   if (g_pFramework->KeyDown (SDLK_DOWN))
+  if (g_pFramework->KeyDown (SDLK_DOWN))
   {
     // Spieler nach unten bewegen
     m_fYPos += 300.0f * g_pTimer->GetElapsed ();
 
+	bHasMoved = true;
   }
  
   // Spieler wurde nicht bewegt
-  else
+  if (!bHasMoved)
   {
     // Animation zurück zum Ausgangspunkt
     if (m_fAnimPhase > 5.0f)