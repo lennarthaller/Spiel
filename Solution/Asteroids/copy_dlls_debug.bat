690eb20dfc213166f30195f78cecffa23c0643f6
unknown <BENUTZEN VERBOTEN@.(none)>
2010-10-10 19:34:37 +0200

    Copy dlls automatically when building the program.

---
+++ Solution/Asteroids/copy_dlls_debug.bat
@@ -0,0 +1,4 @@
+xcopy "C:\Windows\System32\msvcp100d.dll" Debug /D /Y
+xcopy "C:\Windows\System32\msvcr100d.dll" Debug /D /Y
+xcopy "C:\SDL\lib\SDL.dll" Debug /D /Y
+xcopy Data Debug\Data /I /D /Y
---
+++ Solution/Asteroids/copy_dlls_release.bat
@@ -0,0 +1,4 @@
+xcopy "C:\Windows\System32\msvcp100.dll" Release /D /Y
+xcopy "C:\Windows\System32\msvcr100.dll" Release /D /Y
+xcopy "C:\SDL\lib\SDL.dll" Release /D /Y
+xcopy Data Release\Data /I /D /Y
